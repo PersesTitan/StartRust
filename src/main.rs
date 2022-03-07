3f4008264f3ad8ed5833ea8c802c1abc0487e1c8
PersesTitan <torampersestitan@gmail.com>
2022-03-07 19:30:00 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    let a = 1;
+    let mut b = 1;
+    // a = 2; mut 가 없으면 변경 불가능 !!
+    b = 2;
+    const TEST_C: i32 = 1;
+    //const 는 타입 지정이 필요함!!
+    //그나저나 어떻게 타입이 i32...
+    // c = 2; const 도 변경 불가능
+
+    println!("{}", a);
+    println!("{}", b);
+    println!("{}", TEST_C);
 }
